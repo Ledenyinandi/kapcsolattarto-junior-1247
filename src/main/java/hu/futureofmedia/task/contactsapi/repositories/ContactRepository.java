6f568dff2e32d963905a72f6f959fd4effff5ffd
Ledenyinandi <ledenyinandi@gmail.com>
2021-09-22 12:46:34 +0200

    Create ContactRepository

---
+++ src/main/java/hu/futureofmedia/task/contactsapi/repositories/ContactRepository.java
@@ -0,0 +1,13 @@
+package hu.futureofmedia.task.contactsapi.repositories;
+
+import hu.futureofmedia.task.contactsapi.entities.Contact;
+import org.springframework.data.repository.Repository;
+import java.util.List;
+
+public interface ContactRepository extends Repository<Contact, Long> {
+    List<Contact> findAll();
+    Contact findById(Long id);
+    void save(Contact contact);
+    void update(Contact contact, Long id);
+    void deleteById(Long id);
+}
