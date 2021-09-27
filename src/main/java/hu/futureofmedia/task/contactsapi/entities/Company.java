f5b7cf91cda865a35e75c23798d0548762a69b30
Ledenyinandi <ledenyinandi@gmail.com>
2021-09-27 16:35:55 +0200

    Add all args constructor

--- src/main/java/hu/futureofmedia/task/contactsapi/entities/Company.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/entities/Company.java
@@ -1,5 +1,6 @@
 package hu.futureofmedia.task.contactsapi.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
@@ -8,10 +9,10 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Company {
 
     @Id
     private Long id;
     private String name;
-
 }
