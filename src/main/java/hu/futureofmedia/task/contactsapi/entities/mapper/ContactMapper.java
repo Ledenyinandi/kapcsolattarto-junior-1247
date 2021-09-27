51668b3ce2f44419834a7b325add768890d537b3
Ledenyinandi <ledenyinandi@gmail.com>
2021-09-27 16:41:39 +0200

    Add methods

--- src/main/java/hu/futureofmedia/task/contactsapi/entities/mapper/ContactMapper.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/entities/mapper/ContactMapper.java
@@ -7,6 +7,7 @@ import hu.futureofmedia.task.contactsapi.entities.dto.ContactDto1;
 import hu.futureofmedia.task.contactsapi.entities.dto.ContactDto2;
 import hu.futureofmedia.task.contactsapi.entities.dto.ContactDto3;
 import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
 
 @Component
 public class ContactMapper {
@@ -15,9 +16,25 @@ public class ContactMapper {
         return new ContactDto1(
                 contact.getId(),
                 contact.getLastName() + " " + contact.getFirstName(),
-                contact.getCompany().getName(),
+                contact.getCompany(),
                 contact.getEmail(),
                 contact.getPhoneNumber());
+    }
+
+    public Contact convertToEntityForFindAll(ContactDto1 contactDto1) {
+        int lastSpaceChar = contactDto1.getFullName().lastIndexOf(' ');
+        return new Contact(
+                contactDto1.getId(),
+                contactDto1.getFullName().substring(0, lastSpaceChar),
+                contactDto1.getFullName().substring(lastSpaceChar + 1),
+                contactDto1.getEmail(),
+                contactDto1.getPhoneNumber(),
+                contactDto1.getCompany(),
+                "",
+                Status.ACTIVE,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
     }
 
     public ContactDto2 convertToDtoForFindById(Contact contact) {
@@ -25,12 +42,27 @@ public class ContactMapper {
                 contact.getId(),
                 contact.getLastName(),
                 contact.getFirstName(),
-                contact.getCompany().getName(),
+                contact.getCompany(),
                 contact.getEmail(),
                 contact.getPhoneNumber(),
                 contact.getComment(),
                 contact.getCreateTime(),
                 contact.getUpdateTime()
+        );
+    }
+
+    public Contact convertToEntityForFindById(ContactDto2 contactDto2) {
+        return new Contact(
+                contactDto2.getId(),
+                contactDto2.getLastName(),
+                contactDto2.getFirstName(),
+                contactDto2.getEmail(),
+                contactDto2.getPhoneNumber(),
+                contactDto2.getCompany(),
+                contactDto2.getComment(),
+                Status.ACTIVE,
+                contactDto2.getCreateTime(),
+                contactDto2.getUpdateTime()
         );
     }
 
@@ -45,5 +77,18 @@ public class ContactMapper {
                 contactDto3.getComment(),
                 status
                 );
+    }
+
+    public ContactDto3 convertToDtoForSave(Contact contact) {
+        return new ContactDto3(
+                contact.getId(),
+                contact.getLastName(),
+                contact.getFirstName(),
+                contact.getCompany().getId(),
+                contact.getEmail(),
+                contact.getPhoneNumber(),
+                contact.getComment(),
+                contact.getStatus()
+        );
     }
 }
